4b6afdd84f9e06ed13f579f3e30783611d02e1bc
Thomas Cipriani <thomas.cipriani@esimple.it>
2017-11-29 18:28:50 +0100

    Te

--- src/Shrub.php
+++ src/Shrub.php
@@ -2,8 +2,22 @@
 use \Twig_Loader_Filesystem;
 use \Twig_Environment;
 class Shrub {
+	private $_twig;
+	private $_context;
+	private $_template;
+
+	function __construct(string $templateDir) {
+		$loader = new Twig_Loader_Filesystem($templateDir);
+		// Base directory
+		// Blocks
+		// Macros
+		// Views
+		// Pages
+	}
 	public function test($bool = true)
 	{
+		print_r(debug_backtrace());
+		return;
 		$loader = new Twig_Loader_Filesystem(__DIR__);
 		$loader->addPath(__DIR__);
 		$twig = new Twig_Environment($loader, array('debug' => true));
